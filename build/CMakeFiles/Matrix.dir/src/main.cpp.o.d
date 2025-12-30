3b4de78c11d3e7c1acfd9bb9cff50657e449b08d
BulgakovDmitry <dima.olympiad11@yandex.ru>
2025-12-30 22:25:48 +0300

    [all] delete trash files

--- build/CMakeFiles/Matrix.dir/src/main.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/Matrix.dir/src/main.cpp.o: \
- /home/dima/Desktop/coding/coding_C++/Matrix/src/main.cpp \
- /usr/include/stdc-predef.h \
- /home/dima/Desktop/coding/coding_C++/Matrix/include/driver.hpp
