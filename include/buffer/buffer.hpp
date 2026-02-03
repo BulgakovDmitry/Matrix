76674718671c1a662c4f8318e5e3581c6bb684e3
BulgakovDmitry <dima.olympiad11@yandex.ru>
2026-02-03 17:55:49 +0300

    [buffer.hpp] add copy-ctor and copy-assignment

--- include/buffer/buffer.hpp
+++ include/buffer/buffer.hpp
@@ -33,8 +33,21 @@ template <std::floating_point T> class Buffer final : public IBuffer<T> {
     Buffer(std::size_t n_rows, std::size_t n_columns)
         : Buffer(n_rows * n_columns) {}
 
-    Buffer(const Buffer &) = delete;
-    Buffer &operator=(const Buffer &) = delete;
+    Buffer(const Buffer & rhs) {
+        Buffer temp(rhs.capacity_);
+        if (rhs.capacity_ != 0) {
+            std::copy_n(rhs.data_, rhs.capacity_, temp.data_);
+        }
+        swap(temp);
+    }
+
+    Buffer &operator=(const Buffer &rhs) {
+        if (this != &rhs) {
+            Buffer temp(rhs);
+            swap(temp);
+        }
+        return *this;
+    }
 
     Buffer(Buffer &&rhs) noexcept { swap(rhs); }
     Buffer &operator=(Buffer &&rhs) noexcept {
